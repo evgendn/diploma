a71a7ca9a35e22ed799a66a105cfd4253022592e
evgeniy <evgeniydein@gmail.com>
2017-05-08 05:37:16 +0500

    Add check collisions, score and little changes

--- src/game/game.py
+++ src/game/game.py
@@ -1,15 +1,8 @@
-import collections
-import os
-import sys
 import pygame
 from random import randint
 from pygame.locals import *
 
 from game_utils import *
-
-
-WINDOW_WIDTH = 404
-WINDOW_HEIGHT = 500
 
 
 class Bird(pygame.sprite.Sprite):
@@ -21,13 +14,18 @@ class Bird(pygame.sprite.Sprite):
         self.x = x
         self.y = y
         self.velocity = Bird.STANDART_SPEED
+        # For stopping bird
+        self.moving = True
         self.limit_speed = -8
-        self.is_lift = False
         self.image = load_png("bluebird-midflap.png")
+        # For reset game
+        self.old_x = x
+        self.old_y = y
 
     def update(self):
-        self.velocity += Bird.GRAVITY
-        self.y += self.velocity
+        if self.moving:
+            self.velocity += Bird.GRAVITY
+            self.y += self.velocity
 
     def lift(self):
         self.velocity -= Bird.STANDART_SPEED * 20
@@ -38,9 +36,14 @@ class Bird(pygame.sprite.Sprite):
         return Rect(self.x, self.y,
                     self.image.get_width(), self.image.get_height())
 
+    def reset(self):
+        self.x = self.old_x
+        self.y = self.old_y
+        self.velocity = Bird.STANDART_SPEED
+        self.moving = True
+
 
 class Pipe(pygame.sprite.Sprite):
-    SCROLL_SPEED = 3
     GAP = 100
     FLUCTUATION = 150
     LOWEST_BORDER = 160
@@ -51,20 +54,26 @@ class Pipe(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.x = x
         self.y = {}
-        self.velocity = 0.1
+        self.velocity = 3
+        # For stopping Pipes
+        self.moving = True
         self.top_image = pygame.transform.rotate(
             load_png("pipe-red.png"), 180)
         self.bottom_image = load_png("pipe-red.png")
         self.reposition()
-        # Flag for scor counting
+        # Flag for score counting
         self.bird_passed = False
+        # For reset game
+        self.old_x = self.x
+        self.old_y = self.y
 
     def update(self):
-        self.x -= Pipe.SCROLL_SPEED
+        if self.moving:
+            self.x -= self.velocity
 
-        if self.x + Pipe.WIDTH < 0:
-            self.reposition()
-            self.x = WINDOW_WIDTH
+            if self.x + Pipe.WIDTH < 0:
+                self.reposition()
+                self.x = Game.WINDOW_WIDTH
 
     def reposition(self):
         self.bird_passed = False
@@ -78,91 +87,125 @@ class Pipe(pygame.sprite.Sprite):
                            Pipe.WIDTH, Pipe.HEIGHT)
         return top_rect, bottom_rect
 
+    def reset(self):
+        self.x = self.old_x
+        self.y = self.old_y
+        self.moving = True
+        self.bird_passed = False
 
-def main():
-    # Screen init.
-    pygame.init()
-    screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-    pygame.display.set_caption("Flappy Bird clone")
-    pygame.mouse.set_visible(False)
 
-    color = (0, 0, 0)
-    background = pygame.Surface(screen.get_size())
-    background = background.convert()
-    background.fill(color)
+class Game:
+    WINDOW_WIDTH = 404
+    WINDOW_HEIGHT = 500
 
-    # Game objects
-    bird = Bird(20, WINDOW_HEIGHT / 2)
-    pipes = [Pipe(WINDOW_WIDTH),
-             Pipe(WINDOW_WIDTH + Pipe.WIDTH + Pipe.GAP),
-             Pipe(WINDOW_WIDTH + (Pipe.WIDTH + Pipe.GAP) * 2)
-        ]
-    clock = pygame.time.Clock()
-    score = 0
+    def __init__(self):
+        # Screen init.
+        pygame.init()
+        self.screen = pygame.display.set_mode((Game.WINDOW_WIDTH,
+                                               Game.WINDOW_HEIGHT))
+        pygame.display.set_caption("Flappy Bird clone")
+        pygame.mouse.set_visible(False)
 
-    screen.blit(background, (0, 0))
-    pygame.display.flip()
+        # Game objects
+        self.bird = Bird(20, Game.WINDOW_HEIGHT / 2)
+        self.pipes = [Pipe(Game.WINDOW_WIDTH),
+                      Pipe(Game.WINDOW_WIDTH + Pipe.WIDTH + Pipe.GAP),
+                      Pipe(Game.WINDOW_WIDTH + (Pipe.WIDTH + Pipe.GAP) * 2)
+                      ]
+        self.clock = pygame.time.Clock()
+        self.score = 0
+        self.start = False
+        self.background = pygame.transform.scale(load_png("background-day.png"),
+                                                 (Game.WINDOW_WIDTH, Game.WINDOW_HEIGHT))
+        self.score_numbers = [load_png("{}.png".format(i)) for i in range(10)]
 
-    # Event loop
-    while True:
-        clock.tick(30)
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                return
-            elif event.type == KEYDOWN:
-                if event.key == K_SPACE:
-                    bird.lift()
+        pygame.display.flip()
 
-        # Check collision
+    def main_loop(self):
+        # Event loop
+        while True:
+            self.clock.tick(30)
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    return
+                elif event.type == KEYDOWN:
+                    # Core gameplay
+                    if event.key == K_SPACE:
+                        self.bird.lift()
+                    # Reset game
+                    elif event.key == K_r:
+                        self.bird.reset()
+                        for pipe in self.pipes:
+                            pipe.reset()
+                        self.score = 0
+
+            # Check collision
+            if self.check_collision():
+                self.stop()
+
+            # Check score
+            # Check position bird, pipes and increment
+            # score if pipe position less then bird.
+            # Shitty solution i suppose.
+            for pipe in self.pipes:
+                if pipe.bird_passed is False:
+                    if pipe.x + Pipe.WIDTH < self.bird.x:
+                        self.score += 1
+                        pipe.bird_passed = True
+
+            # Update bird and pipes
+            self.bird.update()
+            for pipe in self.pipes:
+                pipe.update()
+
+            # Redraw objects
+            # self.screen.fill((0, 0, 0))
+            self.screen.blit(self.background, (0, 0))
+            self.screen.blit(self.bird.image,
+                             (self.bird.x, self.bird.y))
+            for pipe in self.pipes:
+                self.screen.blit(pipe.top_image, (pipe.x, pipe.y["top"]))
+                self.screen.blit(pipe.bottom_image, (pipe.x, pipe.y["bottom"]))
+            self.draw_score()
+
+            # Update display
+            pygame.display.flip()
+
+    def check_collision(self):
         # Window border and bird
-        if bird.y + bird.image.get_height() > WINDOW_HEIGHT:
-            bird.y = WINDOW_HEIGHT - bird.image.get_height()
-            bird.velocity = 0
-        elif bird.y < 0:
-            bird.y = 0
-            bird.velocity = 0
+        if self.bird.y + self.bird.image.get_height() > Game.WINDOW_HEIGHT:
+            self.bird.y = Game.WINDOW_HEIGHT - self.bird.image.get_height()
+            return True
+        elif self.bird.y < 0:
+            self.bird.y = 0
+            return True
 
         # Pipes and bird
-        collision = False
-        for pipe in pipes:
+        for pipe in self.pipes:
             top_rect, bottom_rect = pipe.rect()
-            if bird.rect().colliderect(top_rect)\
-               or bird.rect().colliderect(bottom_rect):
-                collision = True
+            if self.bird.rect().colliderect(top_rect) \
+                    or self.bird.rect().colliderect(bottom_rect):
+                return True
 
-        if collision:
-            pass
+    def stop(self):
+        self.bird.moving = False
+        for pipe in self.pipes:
+            pipe.moving = False
 
-        # Check score
-        # Check position bird and pipes and increment
-        # score of pipe position less then bird.
-        # Shitty solution i suppose.
-        for pipe in pipes:
-            if pipe.bird_passed is False:
-                if pipe.x + Pipe.WIDTH < bird.x:
-                    score += 1
-                    pipe.bird_passed = True
+    def draw_score(self):
+        splitted_score = [int(i) for i in list(str(self.score))]
+        # Almost all sprites has width 24 -> half is 12
+        width = self.score_numbers[0].get_width()
+        offset = (len(splitted_score) * width) / 2
+        # Position of score numbers
+        x = (Game.WINDOW_WIDTH / 2) - offset
+        y = int(Game.WINDOW_HEIGHT / 7)
 
-        print(score)
-
-        # Update bird and pipes
-        bird.update()
-        for pipe in pipes:
-            pipe.update()
-
-
-        # Check game over
-
-        # Redraw objects
-        screen.fill(color)
-        screen.blit(bird.image, (bird.x, bird.y))
-        for pipe in pipes:
-            screen.blit(pipe.top_image, (pipe.x, pipe.y["top"]))
-            screen.blit(pipe.bottom_image, (pipe.x, pipe.y["bottom"]))
-
-        # Update display
-        pygame.display.flip()
+        for number in splitted_score:
+            self.screen.blit(self.score_numbers[number], (x, y))
+            x += width
 
 
 if __name__ == "__main__":
-    main()
+    game = Game()
+    game.main_loop()
--- src/game/game_utils.py
+++ src/game/game_utils.py
@@ -1,16 +1,12 @@
 import os
 import pygame
-import sys
 
 
 def load_png(name):
     fullname = os.path.join("assets", "sprites", name)
     try:
         image = pygame.image.load(fullname)
-        if image.get_alpha() is None:
-            image = image.convert()
-        else:
-            image.convert_alpha()
+        image.convert_alpha()
     except pygame.error:
         print("Cannot load image:", fullname)
         raise SystemExit
